b97b1d57254ae97406725aa861500cd7825191f7
additionalui <uiadditional@gmail.com>
2024-05-26 13:15:38 +0530

    accordion changes

--- src/components/as-accordion/index.js
+++ src/components/as-accordion/index.js
@@ -1,9 +1,6 @@
 /*
  * ASAccordion
  * @version: 2.5.3
- * @author: Additional Sheet
- * @license: Licensed under MIT (https://additionalui.com/docs/license)
- * Copyright 2023 Additional Sheet
  */
 import Component from '../../core/Component';
 
@@ -14,65 +11,78 @@ class ASAccordion extends Component {
 
   init() {
     document.addEventListener('click', (e) => {
-      const $targetEl = e.target;
-      const $accordionEl = $targetEl.closest(this.selector);
-      const $accordionToggleEl = $targetEl.closest('.as-accordion-toggle');
-      const $accordionGroupEl = $targetEl.closest('.as-accordion-group');
-      if ($accordionEl && $accordionGroupEl && $accordionToggleEl) {
-        this._hideAll($accordionEl);
-
-        this.show($accordionEl);
+      const $targetEl = e.target.closest('.as-accordion-toggle');
+      if ($targetEl) {
+        const $accordionEl = $targetEl.closest(this.selector);
+        if ($accordionEl) {
+          const $accordionGroupEl = $targetEl.closest('.as-accordion-group');
+          if ($accordionEl.classList.contains('as-accordion-active')) {
+            this.hide($accordionEl);
+          } else {
+            this._hideAll($accordionEl, $accordionGroupEl);
+            this.show($accordionEl);
+          }
+        }
       }
     });
   }
 
   show($accordionEl) {
     $accordionEl.classList.add('as-accordion-active');
-  
     const $accordionContentEl = $accordionEl.querySelector('.as-accordion-content');
-  
+    // Change text color of accordion title to indigo
+    const $accordionToggleEl = $accordionEl.querySelector('.as-accordion-toggle');
+    $accordionToggleEl.classList.add('text-indigo-600');
+    this._toggleIcons($accordionEl, true);
+
     $accordionContentEl.style.display = 'block';
-    $accordionContentEl.style.height = 0;
-    setTimeout(() => {
-      $accordionContentEl.style.height = `${$accordionContentEl.scrollHeight}px`;
-    });
-  
+    $accordionContentEl.style.height = `${$accordionContentEl.scrollHeight}px`;
+
     this.afterTransition($accordionContentEl, () => {
-      if (!$accordionEl.classList.contains('as-accordion-active')) return;
       $accordionContentEl.style.height = '';
-  
       this._fireEvent('open', $accordionEl);
       this._dispatch('open.hs.accordion', $accordionEl, $accordionEl);
     });
   }
-  
+
   hide($accordionEl) {
     $accordionEl.classList.remove('as-accordion-active');
-  
+    const $accordionToggleEl = $accordionEl.querySelector('.as-accordion-toggle');
+    $accordionToggleEl.classList.remove('text-indigo-600');
     const $accordionContentEl = $accordionEl.querySelector('.as-accordion-content');
-  
+    this._toggleIcons($accordionEl, false);
+
     $accordionContentEl.style.height = `${$accordionContentEl.scrollHeight}px`;
-    setTimeout(() => {
-      $accordionContentEl.style.height = 0;
-    });
-  
+    setTimeout(() => $accordionContentEl.style.height = '0px');
+
     this.afterTransition($accordionContentEl, () => {
-      if ($accordionEl.classList.contains('as-accordion-active')) return;
-      $accordionContentEl.style.display = '';
-  
+      if (!$accordionEl.classList.contains('as-accordion-active')) {
+        $accordionContentEl.style.display = 'none';
+      }
+      $accordionContentEl.style.height = '';
       this._fireEvent('hide', $accordionEl);
       this._dispatch('hide.hs.accordion', $accordionEl, $accordionEl);
     });
   }
-  
 
-  _hideAll($currentaccordionEl) {
-    const $accordionGroupEl = $currentaccordionEl.closest('.as-accordion-group');
+  _toggleIcons($accordionEl, isActive) {
+    const $plusIcon = $accordionEl.querySelector('[data-icon="plus"]');
+    const $minusIcon = $accordionEl.querySelector('[data-icon="minus"]');
 
+    if (isActive) {
+      $plusIcon.classList.add('hidden');
+      $minusIcon.classList.remove('hidden');
+    } else {
+      $plusIcon.classList.remove('hidden');
+      $minusIcon.classList.add('hidden');
+    }
+  }
+
+  _hideAll($currentAccordionEl, $accordionGroupEl) {
     if ($accordionGroupEl.hasAttribute('data-as-accordion-always-open')) return;
 
     $accordionGroupEl.querySelectorAll(this.selector).forEach(($accordionEl) => {
-      if ($currentaccordionEl !== $accordionEl) {
+      if ($currentAccordionEl !== $accordionEl && $accordionEl.classList.contains('as-accordion-active')) {
         this.hide($accordionEl);
       }
     });
@@ -80,4 +90,4 @@ class ASAccordion extends Component {
 }
 
 window.ASAccordion = new ASAccordion();
-document.addEventListener('load', window.ASAccordion.init());
+document.addEventListener('DOMContentLoaded', () => window.ASAccordion.init());
